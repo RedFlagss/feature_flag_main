045246c8a914db173a34020ca9c6e355842b0cd4
Liza Antipatrova <Liza.Antipa@gmail.com>
2026-03-11 01:32:43 +0300

    ликва не поднялась 0-0

---
+++ feature-flag-service/src/main/resources/db/changelog/create/001-create-schema.sql
@@ -0,0 +1,78 @@
+create or replace function set_updated_at()
+returns trigger
+language plpgsql
+as $$
+begin
+  new.updated_at := now();
+return new;
+end;
+$$;
+
+create table organisation(
+    id bigserial primary key,
+    name string unique not null,
+    created_at timestamp default now(),
+    updated_at timestamp
+);
+
+create trigger organization_set_updated_at
+    before update on organisation
+    for each row execute function set_updated_at();
+
+comment on table organization is 'Организация';
+comment on column organization.id is 'id организации';
+comment on column organization.name is 'Название организации';
+comment on column organization.created_at is 'Дата создания организации';
+comment on column organization.created_at is 'Дата последнего обновления организации';
+
+create table organization_node(
+    id bigserial primary key,
+    name string not null,
+    uuid uuid not null,
+    organization bigint not null references organisation(id),
+    path ltree not null,
+    is_service boolean not null,
+    version bigint not null,
+    created_at timestamp default now(),
+    updated_at timestamp
+);
+
+create trigger organization_node_set_update_at
+    before update on organization_node
+    for each row execute function set_updated_at();
+
+comment on table organization_node is 'Звено в дереве иерархии организации';
+comment on column organization_node.id is 'Id звена организации';
+comment on column organization_node.name is 'Название звена организации';
+comment on column organization_node.uuid is 'Уникальный не технический идентификатор звена организации';
+comment on column organization_node.organization is 'Id организации, к которой принадлежит звено';
+comment on column organization_node.path is 'Путь до текущего звена в древовидной иерархии организации';
+comment on column organization_node.is_service is 'Является ли текущее звено сервисом';
+comment on column organization_node.version is 'Версия данных для оптимистичной блокировки';
+comment on column organization_node.created_at is 'Дата создания звена организации';
+comment on column organization_node.updated_at is 'Дата последнего изменения звена организации';
+
+
+
+create table feature_flag(
+    id bigserial primary key,
+    name string not null,
+    value boolean not null,
+    organization_node bigint not null references organization_node(id),
+    version bigint not null ,
+    created_at timestamp default now(),
+    updated_at timestamp
+);
+
+create trigger feature_flag_set_updated_at
+    before update on feature_flag
+    for each row execute  function set_updated_at();
+
+comment on table feature_flag is 'Фича флаг';
+comment on column feature_flag.id is 'Id фича флага';
+comment on column feature_flag.name is 'Название фича флага';
+comment on column feature_flag.value is 'Значение фича флага';
+comment on column feature_flag.organization_node is 'Id звена организации, к которому принадлежит фича флаг';
+comment on column feature_flag.version is 'Версия данных для оптимистичной блокировки';
+comment on column feature_flag.created_at is 'Дата создания фича флага';
+comment on column feature_flag.updated_at is 'Дата последнего изменения фича флага';
